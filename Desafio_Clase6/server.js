2071e30040e9b392f640eb310f287e6772a65484
olguincona <Folguinco@gmail.com>
2022-07-17 19:10:37 -0400

    Arreglo 2.0

--- Desafio_Clase6/server.js
+++ Desafio_Clase6/server.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express();
-let visitas = 0;
 
-app.get('/', (req, res) => {
-    res.send("<h1 style='color: blue'>Bienvenidos al servidor expess</h1>");
+app.get('/productos', (req, res) => {
+    res.send("<h1 style='color: blue'>Productos</h1>");
 });
 
-app.get('/visitas', (req, res) => {
-    visitas++;
-    res.send(`La cantidad de visitas ${visitas}`);
-})
-
-app.get('/fyh', (req, res) => {
-    const fechayHora = new Date(Date.now()).toLocaleString();
-    res.send({fyh: fechayHora});
+app.get('/productoRandom', (req, res) => {
+    res.send("Producto Random");
 })
 
 const PORT = 8081;
