dc8df8e77098d64fda7c3f8f071caf4ba024b7c1
olguincona <Folguinco@gmail.com>
2022-08-08 18:57:06 -0400

    Desafio

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/', (req, res) =>{
-    res.render('index', {
-        titulo: "Titulo", 
-        mensaje:"Mensaje", 
-        autor: "Autor", 
-        version: "1"});
+app.get('/productos', (req, res) => {
+    res.render('productos', {
+        title: 'Coderhouse App',
+        msg: 'Este es un mensaje, este es el motor pug'
+    });
 })
+
+
+app.post('/productos', (req, res) =>{
+    res.render('productos', {
+        title: "Titulo", 
+        msg:"Mensaje", 
+        mail: "Mail",
+    }); 
+})
+
+
 const PORT = process.env || 8080;
 app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`)
